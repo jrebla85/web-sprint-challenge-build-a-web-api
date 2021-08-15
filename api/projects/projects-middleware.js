c4fa04ecf0f2d7cb56dea50db3ceb61282cc2fb9
jeremy black <jrebla85@yahoo.com>
2021-08-15 17:25:55 -0500

    first two endpoints completed in projects router

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,33 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model");
+
+const checkId = async (req, res, next) => {
+    const {id} = req.params
+
+    try{
+        const project = await Projects.get(id);
+        if(!project){
+            res.status(404).json({ message: `No project with id: ${id}` })
+        }else{
+            req.project = project;
+            next();
+        }
+    }catch(message){
+        res.status(500).json({ error: message })
+    }
+};
+
+const checkBody = (req, res, next) => {
+    const name = req.body;
+    const description = req.body;
+    if(!name || !description || !name.trim() || !description.trim()){
+        res.status(400).json({ message: "missing required name and description field" })
+    }else{
+        next();
+    }
+}
+
+module.exports = {
+    checkId,
+    checkBody
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,30 +3,39 @@ const express = require("express");
 
 const Projects = require("./projects-model");
 const Actions = require("../actions/actions-model");
+const { checkId,
+        checkBody } = require("./projects-middleware");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
+    Projects.get()
+    .then(projects => {
+        if(!projects){
+            res.status(404).json([])
+        }else{
+            res.json(projects)
+        }
+    })
 });
 
-router.get("/:id", (req, res) => {
-
+router.get("/:id", checkId, (req, res) => {
+    res.json(req.project)
 });
 
 router.post("/", (req, res) => {
 
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", checkId, checkBody, (req, res) => {
 
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", checkId, (req, res) => {
 
 });
 
-router.get("/:id/actions", (req, res) => {
+router.get("/:id/actions", checkId, (req, res) => {
 
 });
 
