f3923c819b48ae91e84f767bd3a3f2329e0b0615
jeremy black <jrebla85@yahoo.com>
2021-08-15 16:33:56 -0500

    router skeletons written and exported

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require("cors");
 const helmet = require("helmet");
 
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router")
+
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
+server.use("api/actions", actionsRouter);
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
